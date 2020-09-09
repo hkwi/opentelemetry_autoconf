c7aacaf23deb48b64e5874853776f12e9ef975a1
Hiroaki KAWAI <hiroaki.kawai@gmail.com>
2020-09-10 03:30:36 +0900

    make jaeger optional

--- opentelemetry_autoconf.py
+++ opentelemetry_autoconf.py
@@ -2,7 +2,6 @@ import os
 import logging
 import pkg_resources
 import opentelemetry
-import opentelemetry.exporter.jaeger
 import opentelemetry.sdk.trace.export
 
 logger = logging.getLogger(__name__)
@@ -10,13 +9,38 @@ do_instrument = False
 provider = opentelemetry.trace.get_tracer_provider()
 
 if provider and "OTEL_AUTO_JAEGER_SERVICE_NAME" in os.environ:
-	exporter = opentelemetry.exporter.jaeger.JaegerSpanExporter(
-		service_name=os.environ["OTEL_AUTO_JAEGER_SERVICE_NAME"],
-		agent_host_name=os.environ.get("OTEL_AUTO_JAEGER_AGENT_HOST", "localhost"),
-		agent_port=int(os.environ.get("OTEL_AUTO_JAEGER_AGENT_PORT", "6831"))
+	opts = dict(
+		service_name=os.environ["OTEL_AUTO_JAEGER_SERVICE_NAME"]
 	)
+	if "OTEL_AUTO_JAEGER_AGENT_HOST" in os.environ:
+		opts["agent_host_name"] = os.environ["OTEL_AUTO_JAEGER_AGENT_HOST"]
+	
+	if "OTEL_AUTO_JAEGER_AGENT_PORT" in os.environ:
+		opts["agent_port"] = int(os.environ["OTEL_AUTO_JAEGER_AGENT_PORT"])
+	
+	if "OTEL_AUTO_JAEGER_COLLECTOR_HOST" in os.environ:
+		opts["collector_host_name"] = os.environ["OTEL_AUTO_JAEGER_COLLECTOR_HOST"]
+	
+	if "OTEL_AUTO_JAEGER_COLLECTOR_PORT" in os.environ:
+		opts["collector_port"] = int(os.environ["OTEL_AUTO_JAEGER_COLLECTOR_PORT"])
+	
+	if "OTEL_AUTO_JAEGER_COLLECTOR_ENDPOINT" in os.environ:
+		opts["collector_endpoint"] = os.environ["OTEL_AUTO_JAEGER_COLLECTOR_ENDPOINT"]
+	
+	if "OTEL_AUTO_JAEGER_COLLECTOR_PROTOCOL" in os.environ:
+		opts["collector_protocol"] = os.environ["OTEL_AUTO_JAEGER_COLLECTOR_PROTOCOL"]
+	
+	if "OTEL_AUTO_JAEGER_USERNAME" in os.environ:
+		opts["username"] = os.environ["OTEL_AUTO_JAEGER_USERNAME"]
+	
+	if "OTEL_AUTO_JAEGER_PASSWORD" in os.environ:
+		opts["password"] = os.environ["OTEL_AUTO_JAEGER_PASSWORD"]
+	
+	import opentelemetry.exporter.jaeger
 	provider.add_span_processor(
-		opentelemetry.sdk.trace.export.BatchExportSpanProcessor(exporter)
+		opentelemetry.sdk.trace.export.BatchExportSpanProcessor(
+			opentelemetry.exporter.jaeger.JaegerSpanExporter(**opts)
+		)
 	)
 	do_instrument = True
 
--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from setuptools import setup
 
 setup(
-        name="opentelemetry-autoconf",
-        version="0.1",
-        description="opentelemetry autoconf",
-        py_modules=["opentelemetry_autoconf"],
-        packages=[""],
-        package_data={"":["opentelemetry_autoconf.pth"]},
+	name="opentelemetry-autoconf",
+	version="0.1",
+	description="opentelemetry autoconf",
+	py_modules=["opentelemetry_autoconf"],
+	packages=[""],
+	package_data={"":["opentelemetry_autoconf.pth"]},
+	install_requires=[
+		"opentelemetry-sdk",
+	],
+	extras_require = {
+		"jaeger": ["opentelemetry-exporter-jaeger"]
+	}
 )
